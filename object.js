a81751b553eefe9efb728e36a91880f740056626
LuisCristovao <luis.cristovao92@gmail.com>
2019-12-29 15:13:53 +0000

    engine start

---
+++ object.js
@@ -0,0 +1,26 @@
+class Object {
+    /*
+    This object has two elements
+    - shape
+    - animtion
+    - id
+    */
+    constructor(id){
+        
+        this.id = id
+        //this.shape=createElement("div")
+    }
+    
+    show() {
+        document.body.appendChild(this.shape)
+    }
+    appendChild(element){
+        this.shape.appendChild(element)
+        return this
+    }
+    append(element){
+        this.shape=element
+        return this
+    }
+    
+}
