aaa5cd74ea884d9c92e8286cebd744e425bdd729
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2021-07-17 18:27:20 +0100

    Update level_creation.js

--- EscapeRoom/level_creation.js
+++ EscapeRoom/level_creation.js
@@ -1,6 +1,7 @@
 //global var------
 var square_size=100
-var down=false
+var left_down=false
+var right_down=false
 const matrix_size=100
 const level_objects = new Array(matrix_size).fill(0).map(() => new Array(matrix_size).fill(0));
 var player=new Object().append(createSquare(50*square_size,50*square_size,square_size,square_size,"rgb(0,255,0)","player")).show()
@@ -16,11 +17,21 @@ class Level{
 //functions---
 
 function mouseDown(e) {
-    down = true;
+    if(e.button === 0){
+        left_down = true;
+    }
+    if(e.button === 2){
+        right_down = true;
+    }
 }
 
-function mouseUp() {
-    down = false;
+function mouseUp(e) {
+    if(e.button === 0){
+        left_down = false;
+    }
+    if(e.button === 2){
+        right_down = false;
+    }
 }
 function createLevel(){
     for(let row=0;row<level_objects.length;row++){
@@ -37,16 +48,18 @@ function showLevel(){
     for(let row=0;row<level_objects.length;row++){
       for(let col=0;col<level_objects[row].length;col++){
         if(level_objects[row][col].identity=="1"){
-          level_objects[row][col].object=new Object()
-          .append(createSquare(row*square_size, col*square_size, square_size, square_size, "rgb(0,0,0)"))
-          .show()
-          .shape.onclick=()=>{draw(row,col)}
+            let map_block=new Object()
+            map_block.append(createSquare(row*square_size, col*square_size, square_size, square_size, "rgb(0,0,0)"))
+            .show()
+            map_block.shape.onclick=()=>{draw(row,col)}
+            map_block.shape.onmouseover=()=>{draw(row,col)}
+            level_objects[row][col].object=map_block
         }
         else{
           let map_block=new Object()
           map_block.append(createSquare(row*square_size, col*square_size, square_size, square_size, "rgb(255,255,255)"))
           .show()
-
+          map_block.shape.onclick=()=>{draw(row,col)}
           map_block.shape.onmouseover=()=>{draw(row,col)}
           level_objects[row][col].object=map_block
         }
@@ -54,10 +67,15 @@ function showLevel(){
     }
 }
 function draw(x,y){
-    if(down){
-        let block=level_objects[x][y]
+    let block=level_objects[x][y]
+    if(left_down){
+        
         block.identity="1"
         block.object.shape.style["background-color"]="rgb(0,0,0)"
+    }
+    if(right_down){
+        block.identity="0"
+        block.object.shape.style["background-color"]="rgb(255,255,255)"
     }
 
 }
