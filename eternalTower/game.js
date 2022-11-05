bdd8edcc58d21541ca8221ee43987cf1779e479b
LuisCristovao <luis.cristovao92@gmail.com>
2022-11-05 20:41:59 +0000

    Update game.js

--- eternalTower/game.js
+++ eternalTower/game.js
@@ -3,6 +3,11 @@
 let player=new Character()
 let speed=4
 let up=false,down=false,right=false,left=false
+let ghost_colliders=[]
+let colliders=[]
+ghost_colliders.push(new Character().append(createSquare(100, 100, 100, 100, "rgb(100,100,100,1)")))
+ghost_colliders.push(new Character().append(createSquare(500, 500, 100, 100, "rgb(100,100,100,1)")))
+colliders.push(new Character().append(createSquare(300, 200, 100, 100, "rgb(0,0,0,1)")))
 player.append(createSquare(100, 100, 100, 100, "rgb(255,0,0,1)"))
 player.appendAnimation((self)=>{
   let new_x=self.x;
@@ -20,6 +25,25 @@ player.appendAnimation((self)=>{
     new_x=self.x-speed
   }
   self.move(new_x,new_y)
+  for(let i=0;i<ghost_colliders.length;i++){
+    let c=ghost_colliders[i]
+    if((self.x+self.width>=c.x && self.x<=c.x+c.width) && (self.y+self.height>=c.x && self.y<=c.y+c.height) ){
+      c.shape.style["background-color"]="rgba(0,255,0,1)"
+      setTimeout(()=>{
+        c.shape.style["background-color"]="rgba(100,100,100,1)"
+      },100)
+    }
+  }
+  for(let i=0;i<colliders.length;i++){
+    let c=colliders[i]
+    if((self.x+self.width>=c.x && self.x<=c.x+c.width) && (self.y+self.height>=c.x && self.y<=c.y+c.height) ){
+      new_x=self.x+((self.x))-(c.x)
+      new_y=self.y+(self.y)-(c.y+c.height)
+
+    }
+  }
+  self.move(new_x,new_y)
+  
 })
 document.addEventListener("keydown", press);
 function press(e) {
