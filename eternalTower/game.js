5f9e3165015e6432a7650c2b7c12959a28618065
LuisCristovao <luis.cristovao92@gmail.com>
2022-11-07 22:06:16 +0000

    Update game.js
    projectiles

--- eternalTower/game.js
+++ eternalTower/game.js
@@ -3,12 +3,25 @@
 let player=new Character()
 let speed=4
 let up=false,down=false,right=false,left=false
+
+let projectile_up=false
+
+
 let ghost_colliders=[]
+
 let colliders=[]
+
+let projectiles=[]
+
+
 ghost_colliders.push(new Character().append(createSquare(100, 100, 100, 100, "rgb(100,100,100,1)")))
+
 ghost_colliders.push(new Character().append(createSquare(500, 500, 100, 100, "rgb(100,100,100,1)")))
+
 colliders.push(new Character().append(createSquare(300, 200, 100, 100, "rgb(0,0,0,1)")))
+
 player.append(createSquare(100, 100, 100, 100, "rgb(255,0,0,1)"))
+
 player.appendAnimation((self)=>{
   let new_x=self.x;
   let new_y=self.y;
@@ -61,6 +74,22 @@ player.appendAnimation((self)=>{
       
     }
   }
+  if(projectile_up){
+    let p=new Character().append(createSquare(self.x, self.y, 10, 10, "rgb(100,0,0,1)"))
+    p.appendAnimation((self)=>{
+      for(let i=0;i<ghost_colliders.length;i++){
+        let c=ghost_colliders[i]
+        if((self.x+self.width>=c.x && self.x<=c.x+c.width) && (self.y+self.height>=c.y && self.y<=c.y+c.height) ){
+          c.shape.style["background-color"]="rgba(0,255,0,1)"
+          setTimeout(()=>{
+            c.shape.style["background-color"]="rgba(100,100,100,1)"
+          },100)
+        }
+      }
+      self.move(self.x,self.y-1)
+    })
+    projectiles.push(p)
+  }
   if(self.up){
     new_y=self.y-speed
   }
@@ -80,10 +109,10 @@ player.appendAnimation((self)=>{
 })
 document.addEventListener("keydown", press);
 function press(e) {
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */) {
+  if ( e.keyCode === 87 /* w */) {
     up = true;
   }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */) {
+  if ( e.keyCode === 68 /* d */) {
     right = true;
   }
   if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */) {
@@ -98,10 +127,16 @@ function press(e) {
       dodge = false;
     }, 100);
   }
+  if (e.keyCode === 38 /* up */ ) {
+    projectile_up=true
+  }
+  if(e.keyCode === 39 /* right */ ){
+
+  }
 }
 document.addEventListener("keyup", release);
 function release(e) {
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */) {
+  if ( e.keyCode === 87 /* w */) {
     up = false;
   }
   if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */) {
@@ -113,9 +148,12 @@ function release(e) {
   if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */) {
     left = false;
   }
-   if(e.keyCode === 75/*k key*/){
+  if(e.keyCode === 75/*k key*/){
     dodge = false
-  } 
+  }
+  if (e.keyCode === 38 /* up */ ) {
+    projectile_up=false
+  }
 }
 
 
@@ -128,7 +166,10 @@ function init(){
 }
 
 function main(){
-  player.play()    
+  player.play()
+  projectiles.forEach(p=>{
+    p.play()
+  })   
   requestAnimationFrame(main)
 }
 //Main ---
