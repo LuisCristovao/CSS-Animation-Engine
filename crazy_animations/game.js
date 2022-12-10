6c01ed98385ead42dde6e8f258fbd176b5d6110f
LuisCristovao <luis.cristovao92@gmail.com>
2022-12-10 19:01:00 +0000

    Update game.js

--- crazy_animations/game.js
+++ crazy_animations/game.js
@@ -4,10 +4,14 @@ let dt = 0;
 let n_particles=300
 let x_equation=`x-2*sin(1*(100*(t/(i*0.2))-0.3*i))`
 let y_equation=`y-2*cos(1*(100*(t/(i*0.2))-0.3*i))`
+let color_equation=`color-10*sin(0.2*(2*t)-i)`
 let particles = [];
 let animtion_id=0
 let menu_on=false
 function initSite(){
+  if(window.location.search!=''){
+    
+  }
   document.body.style.backgroundColor = "hsl(0,50%,5%)";
   exitMenu()
   RunAnimation()
@@ -15,9 +19,10 @@ function initSite(){
 function Menu(){
   menu_on=true
   //cancelAnimation()
-  let html=`<pre style="color:white">x = x position \ny = y position \nt = time \ni = particle number</pre>`
+  let html=`<pre style="color:white">x = x position \ny = y position \nt = time\ncolor = particle color(hue) \ni = particle number</pre>`
   html+=`<input oninput="changeXequation(this.value)" type="text" placeholder="Input X Equation"  value="${x_equation}"><br>`
   html+=`<input oninput="changeYequation(this.value)" type="text" placeholder="Input Y Equation"  value="${y_equation}"><br>`
+  html+=`<input oninput="changeColorequation(this.value)" type="text" placeholder="Input Color Equation"  value="${color_equation}"><br>`
   html+=`<input oninput="changeNumberOfParticles(this.value)" type="number" placeholder="Number of Particles" value="${n_particles}"><br>`
   html+=`<button onclick="exitMenu()">Exit Menu</button>`
   document.body.innerHTML=html
@@ -43,6 +48,11 @@ function changeYequation(new_equation){
   y_equation=new_equation
   createParticles() 
 }
+function changeColorequation(new_equation){
+  deleteParticles()
+  color_equation=new_equation
+  createParticles() 
+}
 function transformEquation(equation){
   return equation
   .replaceAll("t","particles[i].real_time")
@@ -50,6 +60,7 @@ function transformEquation(equation){
   .replaceAll("y","particles[i].y")
   .replaceAll("sin","Math.sin")
   .replaceAll("cos","Math.cos")
+  .replaceAll("color","particles[i].color[0]")
 }
 function createParticles(){
   for (i = 0; i <n_particles; i++) {
@@ -63,8 +74,13 @@ function createParticles(){
       }catch(error){
         console.log(error)
       }
+      try{
+        eval(`particles[i].setColor([${transformEquation(color_equation)},particles[i].color[1],particles[i].color[2],1])`)
+      }catch(error){
+        console.log(error)
+      }
       //particles[i].move(particles[i].x-2*Math.sin(1*(100*(particles[i].real_time/(i*0.2))-0.3*i)),particles[i].y-2*Math.cos((1)*(100*(particles[i].real_time/(i*0.2))-0.3*i)))
-      particles[i].setColor([particles[i].color[0]-10*Math.sin(0.2*(2*particles[i].real_time)-i),particles[i].color[1],particles[i].color[2],1]);
+      //particles[i].setColor([particles[i].color[0]-10*Math.sin(0.2*(2*particles[i].real_time)-i),particles[i].color[1],particles[i].color[2],1]);
     });
     
   }
