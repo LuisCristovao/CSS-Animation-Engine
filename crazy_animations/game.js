e754df352639d431098137a793ebf15e9c9cfa22
LuisCristovao <luis.cristovao92@gmail.com>
2022-12-11 11:26:38 +0000

    Update game.js

--- crazy_animations/game.js
+++ crazy_animations/game.js
@@ -47,6 +47,7 @@ function Menu(){
 }
 function ShareEquations(btn){
   btn.innerText="Copied Link!"
+  
   copyToClipboard(window.location.origin+`/CSS-Animation-Engine/crazy_animations/game.html?${x_equation};${y_equation};${color_equation}`)
   setTimeout(()=>{
     btn.innerText="Share Equations"
