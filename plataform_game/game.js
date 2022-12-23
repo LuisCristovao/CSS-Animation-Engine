f953ef110f5491e6aae1bfbbaf23d85cc86ac236
LuisCristovao <luis.cristovao92@gmail.com>
2022-12-23 19:41:14 +0000

    Update game.js

--- plataform_game/game.js
+++ plataform_game/game.js
@@ -1,7 +1,7 @@
 let real_time = 0;
 let time = new Date().getTime();
 let dt = 0;
-let offset = 100000
+let offset = 100000;
 let player = new Object();
 let speed = 4;
 let up = false,
@@ -11,9 +11,8 @@ let up = false,
   jump = false;
 
 let colliders = [];
-let mouse = [0, 0]
-let mouse_obj = new Object()
-
+let mouse = [0, 0];
+let mouse_obj = new Object();
 
 player.append(createSquare(offset, offset, 100, 100, "rgb(255,0,0,1)"));
 //player.appendChild(createSquare(player.x-1000, player.y-1000, 1000, 1000, "rgb(255,255,255,0.1)","window"))
@@ -22,34 +21,43 @@ document.body.addEventListener("touchstart", touchStart, false);
 document.body.addEventListener("touchend", touchEnd, false);
 
 function touchEnd(e) {
-  mouse_obj.destroy()
-  up = false
-  down = false
-  right = false
-  left = false
-  jump = false
+  mouse_obj.destroy();
+  up = false;
+  down = false;
+  right = false;
+  left = false;
+  jump = false;
 }
 function touchStart(e) {
   mouse[0] = e.touches[0].clientX;
   mouse[1] = e.touches[0].clientY;
-  mouse_obj.append(createCircle(e.touches[0].clientX + player.x - window.innerWidth / 2, e.touches[0].clientY + player.y - window.innerHeight / 2, 100, "rgb(0,0,255,0.4)"))
+  mouse_obj.append(
+    createCircle(
+      e.touches[0].clientX + player.x - window.innerWidth / 2,
+      e.touches[0].clientY + player.y - window.innerHeight / 2,
+      100,
+      "rgb(0,0,255,0.4)"
+    )
+  );
 }
 
 function touchMove(e) {
-  mouse_obj.move(e.touches[0].clientX + player.x - window.innerWidth / 2, e.touches[0].clientY + player.y - window.innerHeight / 2)
+  mouse_obj.move(
+    e.touches[0].clientX + player.x - window.innerWidth / 2,
+    e.touches[0].clientY + player.y - window.innerHeight / 2
+  );
   if (mouse[1] - e.touches[0].clientY > 200) {
     up = true;
   }
   if (mouse[0] - e.touches[0].clientX < 0) {
-    right = true
+    right = true;
   }
   if (mouse[0] - e.touches[0].clientX > 0) {
-    left = true
+    left = true;
   }
-  console.log(mouse[1] - e.touches[0].clientY)
+  //console.log(mouse[1] - e.touches[0].clientY)
   //alert(mouse)
 }
-
 
 //----------------
 //collision detection function------
@@ -76,10 +84,8 @@ function collisionDetection(object1, object2, action, solid = true) {
       object1.y <= object2.y + object2.height
     ) {
       action(object1, object2);
-      let collision_margin_error = 10
-      if (
-        object1.x - object2.x < 0
-      ) {
+      let collision_margin_error = 10;
+      if (object1.x - object2.x < 0) {
         left_of = true;
         aux_orientation++;
       }
@@ -88,15 +94,11 @@ function collisionDetection(object1, object2, action, solid = true) {
         //console.log("right of " + (object1.x - object2.x))
         aux_orientation++;
       }
-      if (
-        object1.y - object2.y > 0
-      ) {
+      if (object1.y - object2.y > 0) {
         below = true;
         aux_orientation++;
       }
-      if (
-        object1.y - object2.y < 0
-      ) {
+      if (object1.y - object2.y < 0) {
         //console.log('above ' + (object1.y - object2.y))
         above = true;
         aux_orientation++;
@@ -104,38 +106,47 @@ function collisionDetection(object1, object2, action, solid = true) {
 
       if (aux_orientation > 1) {
         if (Math.abs(object1.y - object2.y) > Math.abs(object1.x - object2.x)) {
-          left_of = false; right_of = false
+          left_of = false;
+          right_of = false;
         } else {
-          above = false; below = false
+          above = false;
+          below = false;
         }
       }
 
-
-
       if (below) {
         object1.up = false;
-        object1.speedy = (object1.y - object2.y) * 0.05
-        object1.options.on_ground = false
+        object1.speedy = (object1.y - object2.y) * 0.05;
+        object1.options.on_ground = false;
       }
       if (above) {
         object1.down = false;
-        object1.speedy = (object1.y - object2.y) * 0.05
-
+        object1.speedy = (object1.y - object2.y) * 0.05;
+        //console.log((object1.y - object2.y))
       }
       if (right_of) {
-        object1.left = false;
-        object1.speedx = (object1.x - object2.x) * 0.05
-        object1.options.on_ground = false
+        if (
+          object1.height - Math.abs(object1.y - object2.y) >
+          object1.height * 0.1
+        ) {
+          object1.left = false;
+          object1.speedx = (object1.x - object2.x) * 0.05;
+          object1.options.on_ground = false;
+        }
       }
       if (left_of) {
-        object1.right = false;
-        object1.speedx = (object1.x - object2.x) * 0.05
-        object1.options.on_ground = false
+        if (
+          object1.height - Math.abs(object1.y - object2.y) >
+          object1.height * 0.1
+        ) {
+          object1.right = false;
+          object1.speedx = (object1.x - object2.x) * 0.05;
+          object1.options.on_ground = false;
+        }
       }
     }
   }
 }
-
 
 player.appendAnimation((self) => {
   let new_x = self.x;
@@ -148,8 +159,6 @@ player.appendAnimation((self) => {
   self.left = false;
   self.right = false;
   self.options.on_ground = false;
-
-
 
   if (up) {
     self.up = true;
@@ -175,42 +184,44 @@ player.appendAnimation((self) => {
           solid_block.shape.style["background-color"] = "rgba(0,0,0,1)";
         }, 100);
         player.options.on_ground = true;
-        player.options.double_jump = false
+        player.options.double_jump = false;
         player.real_time = 0;
       },
       (solid = true)
     );
   }
 
-  if ((self.up && self.options.on_ground) || (self.up && !self.options.double_jump && !self.options.on_ground)) {
+  if (
+    (self.up && self.options.on_ground) ||
+    (self.up && !self.options.double_jump && !self.options.on_ground)
+  ) {
     self.speedy = self.dt * -15000;
     if (self.up && !self.options.double_jump && !self.options.on_ground) {
-      self.options.double_jump = true
+      self.options.double_jump = true;
     }
-
   }
   if (self.down) {
     //new_y = self.y + speed;
   }
   if (self.right) {
-    self.speedx = self.speedx + self.dt * ((self.options.on_ground)?900:150);
+    self.speedx = self.speedx + self.dt * (self.options.on_ground ? 900 : 150);
   }
   if (self.left) {
-    self.speedx = self.speedx + self.dt * ((self.options.on_ground)?-900:-150);
+    self.speedx =
+      self.speedx + self.dt * (self.options.on_ground ? -900 : -150);
   }
   //on the air
   if (!self.options.on_ground) {
     self.speedy = self.speedy + self.dt * 300;
-  }else{
+  } else {
     //x-axis drag on floor
-    self.speedx=self.speedx+(-self.speedx*0.07)
+    self.speedx = self.speedx + -self.speedx * 0.07;
   }
 
-  console.log(self.speedx)
+  //console.log(self.speedy)
 
   self.move(self.x + self.dt * self.speedx, self.y + self.dt * self.speedy);
-  
-  
+
   //self.move(new_x,new_y)
 });
 document.addEventListener("keydown", press);
@@ -290,41 +301,58 @@ function cleanUnusedProjectiles() {
 function init() {
   document.body.style.background = "rgb(0,0,0)";
   document.body.style.overflow = "hidden";
-  document.body.style.height = "200000px"
-  document.body.style.width = "2000000px"
+  document.body.style.height = "200000px";
+  document.body.style.width = "2000000px";
   let length = 100;
   let box_size = 20;
-  let c = new Object().append(
-    createSquare(player.x, player.y + player.height, length, length, "rgb(0,0,0,1)")
-  );
-  c.shape.style.border = "2px solid white";
-  colliders.push(c);
+  let c;
+  //make ground
+  for (i = -50; i < 50; i++) {
+    c = new Object().append(
+      createSquare(
+        player.x + i * length,
+        player.y + player.height,
+        length,
+        length,
+        "rgb(0,0,0,1)"
+      )
+    );
+    c.shape.style.border = "2px solid white";
+    colliders.push(c);
+  }
+
   c = new Object().append(
     createSquare(player.x + 200, player.y, length, length, "rgb(0,0,0,1)")
   );
   c.shape.style.border = "2px solid white";
   colliders.push(c);
   for (i = 0; i < 100; i++) {
-    let random = Math.random()
-    let next_x_position_block = 0
-    let next_y_position_block = 0
+    let random = Math.random();
+    let next_x_position_block = 0;
+    let next_y_position_block = 0;
     if (random > 0.5) {
-      next_x_position_block = 400
+      next_x_position_block = 400;
     } else {
-      next_x_position_block = -400
+      next_x_position_block = -400;
     }
     if (Math.random() > 0.5) {
-      next_y_position_block = 200
+      next_y_position_block = 200;
     } else {
-      next_x_position_block = 100
+      next_x_position_block = 100;
     }
 
     let nc = new Object().append(
-      createSquare(c.x + next_x_position_block, c.y - 100, length, length, "rgb(0,0,0,1)")
+      createSquare(
+        c.x + next_x_position_block,
+        c.y - 100,
+        length,
+        length,
+        "rgb(0,0,0,1)"
+      )
     );
     nc.shape.style.border = "2px solid white";
     colliders.push(nc);
-    c = nc
+    c = nc;
   }
 
   // for (let y = 0; y < box_size; y++) {
